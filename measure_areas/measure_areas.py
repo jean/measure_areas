3802bff262da873c30dab4e98de82068ebf11c23
Jean Jordaan <jean.jordaan@gmail.com>
2017-11-27 20:34:31 +0700

    Naively gather areas

--- measure_areas/measure_areas.py
+++ measure_areas/measure_areas.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from svgpathtools import svg2paths
+
+def measure_areas(filename):
+    """
+    Compute the areas of all paths in the SVG file `filename`
+    """
+    paths, attributes = svg2paths(filename)
+
+    areas = []
+    for p in paths:
+        try:
+            areas.append(p.area())
+        except:
+            import pdb
+            pdb.set_trace()
+
+    print("{0}: {1}".format(filename, abs(sum(areas))))
